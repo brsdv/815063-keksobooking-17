f6dd50934027523748652d316dd8183fe6b00e18
Boris davydenko <brsdv@yandex.ru>
2019-06-13 15:21:34 +0300

    Личный проект: пока все дома - часть 2

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
 'use strict';
 
+var MIN_Y_COORDINATE = 130; // Минимальная координата Y метки на карте
+var MAX_Y_COORDINATE = 630; // Максимальная координата Y метки на карте
+var WIDTH_PIN = 50; // Ширина метки, определяется в CSS
+var HEIGHT_PIN = 70; // Высота метки, определяется в CSS
+
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 var offers = ['palace', 'flat', 'house', 'bungalo'];
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+var mapPinContainer = document.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin')
+  .content;
 
 var getGenerationPin = function () {
   var arrObj = [];
 
-  numbers.sort(function () {
-    return Math.random() - 0.5;
-  });
+  var shuffle = function (arr) {
+    var j;
+    var temp;
+
+    for (var i = arr.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      temp = arr[j];
+      arr[j] = arr[i];
+      arr[i] = temp;
+    }
+
+    return arr;
+  };
+
+  var randomNumbers = shuffle(numbers);
 
   var randomCoordinate = function (min, max) {
     var rand = min - 0.5 + Math.random() * (max - min + 1);
@@ -16,29 +40,51 @@ var getGenerationPin = function () {
     return rand;
   };
 
-  for (var i = 0; i < numbers.length; i++) {
-    var number = String('0') + numbers[i];
-    var randOffer = offers[Math.floor(Math.random() * offers.length)];
+  for (var i = 0; i < randomNumbers.length; i++) {
+    var leadZero = String('0') + randomNumbers[i];
+    var randomOffer = offers[Math.floor(Math.random() * offers.length)];
+    var queryMap = document.querySelector('.map');
 
     var obj = {
       author: {
-        avatar: 'img/avatars/user' + number + '.png'
+        avatar: 'img/avatars/user' + leadZero + '.png'
       },
       offer: {
-        type: randOffer
+        type: randomOffer
       },
       location: {
-        x: randomCoordinate(0, document.querySelector('.map').clientWidth),
-        y: randomCoordinate(130, 630)
+        x: randomCoordinate(queryMap.clientLeft, queryMap.clientWidth),
+        y: randomCoordinate(MIN_Y_COORDINATE, MAX_Y_COORDINATE)
       }
     };
+
     arrObj.push(obj);
   }
 
   return arrObj;
 };
 
-console.log(getGenerationPin());
+var pins = getGenerationPin();
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var createPin = function (pin) {
+  var pinElement = pinTemplate.cloneNode(true);
+
+  pinElement.querySelector('img').src = pin.author.avatar;
+  pinElement.querySelector('img').alt = pin.offer.type;
+  pinElement.querySelector('.map__pin').style.left = pin.location.x - WIDTH_PIN / 2 + 'px';
+  pinElement.querySelector('.map__pin').style.top = pin.location.y - HEIGHT_PIN + 'px';
+
+  return pinElement;
+};
+
+var renderPin = function () {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < pins.length; i++) {
+    fragment.appendChild(createPin(pins[i]));
+  }
+
+  return fragment;
+};
+
+mapPinContainer.appendChild(renderPin());
