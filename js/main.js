e71b5d966408c7872b1f475723ac66db2102afaf
Boris davydenko <brsdv@yandex.ru>
2019-06-20 22:07:16 +0300

    Выполнение задания - максимум подвижности

--- js/main.js
+++ js/main.js
@@ -2,30 +2,32 @@
 
 var MIN_Y_COORDINATE = 130; // Минимальная координата Y метки на карте
 var MAX_Y_COORDINATE = 630; // Максимальная координата Y метки на карте
-var WIDTH_PIN = 50; // Ширина метки, определяется в CSS
-var HEIGHT_PIN = 70; // Высота метки, определяется в CSS
+var WIDTH_PIN = 50; // Ширина пользовательской метки, определяется в CSS
+var HEIGHT_PIN = 70; // Высота пользовательской метки, определяется в CSS
 var MAIN_HEIGHT_PIN = 81; // Высота главной метки, определяется метрикой scrollHeight в активном режиме страницы
 var ALT_TEXT_IMG = 'заголовок объявления';
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 var offers = ['palace', 'flat', 'house', 'bungalo'];
 
-var pinContainer = document.querySelector('.map__pins'); // Контейнер для всех меток объявлений
-var pinMainButton = pinContainer.querySelector('.map__pin--main'); // Начальная метка на карте
-var adForm = document.querySelector('.ad-form'); // Форма заполнения объявления
-var addressName = adForm.querySelector('#address'); // Поле ввода адреса
-var startCoordinateX = Math.round(pinMainButton.offsetLeft + pinMainButton.offsetWidth / 2); // Середина начальной метки на карте по оси X
-var startCoordinateY = Math.round(pinMainButton.offsetTop + pinMainButton.offsetHeight / 2); // Середина начальной метки на карте по оси Y
-addressName.setAttribute('value', startCoordinateX + ', ' + startCoordinateY); // Добавляю в поле "адрес" координаты метки в неактивном режиме
-
 var pinTemplate = document.querySelector('#pin')
-  .content;
+  .content; // Шаблон DocumentFragment пользовательской метки на карте
 
+var map = document.querySelector('.map'); // Секция карты
+var pinContainer = document.querySelector('.map__pins'); // Контейнер для всех меток объявлений
+var pinMain = pinContainer.querySelector('.map__pin--main'); // Начальная метка на карте
+var adForm = document.querySelector('.ad-form'); // Форма заполнения объявления
 var titleInput = adForm.querySelector('#title');
-var priceInput = adForm.querySelector('#price');
+var addressInput = adForm.querySelector('#address');
 var typeSelect = adForm.querySelector('#type');
+var priceInput = adForm.querySelector('#price');
 var timeInSelect = adForm.querySelector('#timein');
 var timeOutSelect = adForm.querySelector('#timeout');
+var pinMainHalfWidth = pinMain.offsetWidth / 2; // Середина самой метки по оси X
+var pinMainHalfHeight = pinMain.offsetHeight / 2; // Середина самой метки по оси Y
+var startCoordinateX = Math.round(pinMain.offsetLeft + pinMainHalfWidth); // Середина начальной метки на карте по оси X
+var startCoordinateY = Math.round(pinMain.offsetTop + pinMainHalfHeight); // Середина начальной метки на карте по оси Y
+addressInput.value = startCoordinateX + ', ' + startCoordinateY; // Добавляю в поле "адрес" координаты метки в неактивном режиме
 
 var getGenerationPin = function () {
   var arrObj = [];
@@ -55,7 +57,6 @@ var getGenerationPin = function () {
   for (var i = 0; i < randomNumbers.length; i++) {
     var leadZero = String('0') + randomNumbers[i];
     var randomOffer = offers[Math.floor(Math.random() * offers.length)];
-    var queryMap = document.querySelector('.map');
 
     var obj = {
       author: {
@@ -65,7 +66,7 @@ var getGenerationPin = function () {
         type: randomOffer
       },
       location: {
-        x: randomCoordinate(queryMap.clientLeft, queryMap.clientWidth),
+        x: randomCoordinate(map.clientLeft, map.clientWidth),
         y: randomCoordinate(MIN_Y_COORDINATE, MAX_Y_COORDINATE)
       }
     };
@@ -98,36 +99,6 @@ var renderPin = function () {
 
   return fragment;
 };
-
-var getStatusPage = function (status) {
-  var map = document.querySelector('.map');
-  var fieldsets = adForm.querySelectorAll('fieldset');
-
-  if (!status) {
-    map.classList.remove('map--faded');
-    adForm.classList.remove('ad-form--disabled');
-    pinContainer.appendChild(renderPin());
-  }
-
-  for (var i = 0; i < fieldsets.length; i++) {
-    fieldsets[i].disabled = status;
-
-    if (!status) {
-      fieldsets[i].removeAttribute('disabled');
-    }
-  }
-};
-getStatusPage(true);
-
-var pinCoordinate = function () {
-  var currentCoordinateY = Math.round(pinMainButton.offsetTop + MAIN_HEIGHT_PIN);
-  addressName.setAttribute('value', startCoordinateX + ', ' + currentCoordinateY);
-};
-
-pinMainButton.addEventListener('mouseup', function () {
-  getStatusPage(false);
-  pinCoordinate();
-});
 
 var changeInputPriceHandler = function (val) {
   if (val === 'bungalo') {
@@ -184,4 +155,83 @@ priceInput.addEventListener('invalid', function (evt) {
   } else {
     priceInput.setCustomValidity('');
   }
+});
+
+var getStatusPage = function (status) {
+  var fieldsets = adForm.querySelectorAll('fieldset');
+
+  if (!status) {
+    map.classList.remove('map--faded');
+    adForm.classList.remove('ad-form--disabled');
+    pinContainer.appendChild(renderPin());
+  }
+
+  for (var i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].disabled = status;
+
+    if (!status) {
+      fieldsets[i].removeAttribute('disabled');
+    }
+  }
+};
+getStatusPage(true);
+
+var pinCoordinate = function () {
+  var currentCoordinateX = Math.round(pinMain.offsetLeft + pinMainHalfWidth);
+  var currentCoordinateY = Math.round(pinMain.offsetTop + MAIN_HEIGHT_PIN);
+  addressInput.value = currentCoordinateX + ', ' + currentCoordinateY;
+};
+
+// Добавляю обработчик на mouseDown
+pinMain.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  pinMain.style.zIndex = 10;
+
+  if (map.classList.length === 2) {
+    getStatusPage(false);
+  }
+
+  var startCoordinate = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  // callback mouseMove
+  var mouseMoveHandler = function (evtMove) {
+    evtMove.preventDefault();
+
+    var shift = {
+      x: startCoordinate.x - evtMove.clientX,
+      y: startCoordinate.y - evtMove.clientY
+    };
+
+    startCoordinate = {
+      x: evtMove.clientX,
+      y: evtMove.clientY
+    };
+
+    var currentX = pinMain.offsetLeft - shift.x;
+    var currentY = pinMain.offsetTop - shift.y;
+
+    if (currentX >= map.clientLeft - pinMainHalfWidth && currentX <= map.clientWidth - pinMainHalfWidth) {
+      pinMain.style.left = currentX + 'px';
+    }
+    if (currentY >= MIN_Y_COORDINATE - MAIN_HEIGHT_PIN && currentY <= MAX_Y_COORDINATE - MAIN_HEIGHT_PIN) {
+      pinMain.style.top = currentY + 'px';
+    }
+  };
+
+  // callback mouseUp
+  var mouseUpHandler = function (evtUp) {
+    evtUp.preventDefault();
+
+    pinCoordinate();
+
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+  };
+
+  // Добавляю обработчики на mouseMove и mouseUp
+  document.addEventListener('mousemove', mouseMoveHandler);
+  document.addEventListener('mouseup', mouseUpHandler);
 });
